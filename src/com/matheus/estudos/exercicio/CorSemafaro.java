44ff022d22a13efe125c8ee0d2569993717805dc
matheus-vieiras <matheusvieira.contato@hotmail.com>
2026-01-17 15:13:21 -0300

    Semáfaro com Threads

---
+++ src/com/matheus/estudos/exercicio/CorSemafaro.java
@@ -0,0 +1,16 @@
+package com.matheus.estudos.exercicio;
+
+public enum CorSemafaro {
+
+    VERDE(1000), AMARELO(300), VERMELHO(2000);
+
+    private int tempoEspera;
+
+    CorSemafaro(int tempoEspera) {
+        this.tempoEspera = tempoEspera;
+    }
+
+    public int getTempoEspera() {
+        return tempoEspera;
+    }
+}
---
+++ src/com/matheus/estudos/exercicio/SimuladorSemafaro.java
@@ -0,0 +1,16 @@
+package com.matheus.estudos.exercicio;
+
+public class SimuladorSemafaro {
+
+    public static void main(String[] args) {
+
+        ThreadSemafaro semafaro = new ThreadSemafaro();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(semafaro.getCor());
+            semafaro.esperaCorMudar();
+        }
+
+        semafaro.desligarSemafaro();
+    }
+}
